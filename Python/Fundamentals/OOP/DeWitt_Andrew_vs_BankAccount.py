8d1112da84ae4c35cad87b334fa424dc8a7e01c9
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2021-10-04 22:28:54 -0700

    10/5 part2

--- Python/Fundamentals/OOP/DeWitt_Andrew_vs_BankAccount.py
+++ Python/Fundamentals/OOP/DeWitt_Andrew_vs_BankAccount.py
@@ -1,26 +1,40 @@
 class BankAccount:
-
+    bankName = 'First National Dojo'
+    allAccounts = []
     def __init__(self, name, intRate, balance):
         self.name = name
-        self.accountBalance = balance
-        self.interestRate = intRate/1000
+        self.intRate = intRate/1000
+        self.balance = balance
+        BankAccount.allAccounts.append(self)
 
     def deposit(self, amount):
-        self.accountBalance += amount
+        self.balance += amount
         return self
 
     def withdrawal(self, amount):
-        self.accountBalance -= amount
+        self.balance -= amount
         return self
 
-    def displayBalance(self):
-        print(self.name, 'Balance: $' + str(self.accountBalance))
-
     def yieldInterest(self):
-        self.accountBalance *= self.interestRate
+        self.balance += (self.balance * self.intRate)
+        return self
     
     def displayAccountInfo(self):
-        print(self.name, self.interestRate, self.accountBalance)
+        print('Balance: $' + str(self.balance))
+
+    @classmethod
+    def defaultBalance(cls):
+        savings = 0
+        for account in cls.allAccounts:
+            savings += account.balance
+        return savings
 
 
 Phoenix = BankAccount('Andrew', 15, 2348)
+Guido = BankAccount('Guido', 10, 23)
+
+Phoenix.deposit(340).deposit(4620).deposit(324).withdrawal(4503).yieldInterest()
+print(Phoenix.displayAccountInfo())
+
+Guido.deposit(3446).deposit(3000).withdrawal(21).withdrawal(657).withdrawal(345).withdrawal(32).yieldInterest()
+print(Guido.displayAccountInfo())
